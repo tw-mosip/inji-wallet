a1d6bbb64ceb198e4e07f035c284f582171ced38
Kiruthika Jeyashankar <81218987+KiruthikaJeyashankar@users.noreply.github.com>
2023-09-06 11:04:05 +0530

    refactor(INJI-302): use custom header in issuer & error screen

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { Text, TouchableOpacity, View } from 'react-native';
 import { Icon } from 'react-native-elements';
-import { HomeRouteProps } from '../../routes/main';
 import { Column, Row } from './Layout';
 import { Theme } from './styleUtils';
 
-export const Header: React.FC<HeaderProps> = ({ navigation, title }) => {
+export const Header: React.FC<HeaderProps> = ({ goBack, title }) => {
   return (
     <Column safe align="center">
       <Row elevation={2}>
@@ -18,11 +17,11 @@ export const Header: React.FC<HeaderProps> = ({ navigation, title }) => {
             marginBottom: 22,
             marginVertical: 16,
           }}>
-          <TouchableOpacity onPress={navigation.goBack}>
+          <TouchableOpacity onPress={goBack}>
             <Icon
               name="arrow-left"
               type="material-community"
-              onPress={navigation.goBack}
+              onPress={goBack}
               containerStyle={{
                 ...Theme.Styles.backArrowContainer,
                 marginLeft: 10,
@@ -45,5 +44,5 @@ export const Header: React.FC<HeaderProps> = ({ navigation, title }) => {
 
 interface HeaderProps {
   title: string;
-  navigation: HomeRouteProps['navigation'];
+  goBack: () => void;
 }
