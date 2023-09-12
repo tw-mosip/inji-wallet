01383d78c5df683aa0ef5fb6b91625ea9094c584
Kiruthika Jeyashankar <81218987+KiruthikaJeyashankar@users.noreply.github.com>
2023-09-12 17:21:50 +0530

    fix(INJI-245): show download card button when no vcs are there
    When a VC is downloaded and removed from wallet 'Download card' button was not visible given ENABLE_OPENID_FOR_VC is toggled off. This is fixed here.

--- shared/openId4VCI/Utils.ts
+++ shared/openId4VCI/Utils.ts
@@ -2,7 +2,7 @@ import { ENABLE_OPENID_FOR_VC } from 'react-native-dotenv';
 
 export const OpenId4VCIProtocol = 'OpenId4VCI';
 export const isVCFromOpenId4VCI = (vcKey: string) => {
-  return vcKey?.indexOf('urn') != -1;
+  return vcKey?.indexOf('urn') !== -1;
 };
 
 export const isOpenId4VCIEnabled = () => {
