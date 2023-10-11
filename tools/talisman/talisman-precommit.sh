48fb6831836e2ac096d2822e22d37f2a5f8a9f39
Pooja Babusingh <68894211+PoojaBabusingh@users.noreply.github.com>
2023-10-11 13:06:21 +0530

    talismann check 7

--- tools/talisman/talisman-precommit.sh
+++ tools/talisman/talisman-precommit.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-echo ">>>>>>>>> Copies Talisman pre-commit and pre-push hooks to .git folder"
 preCommitFile='.git/hooks/pre-commit'
 prePushFile='.git/hooks/pre-push'
 gitFolder='.git'
@@ -11,33 +9,27 @@ then
 else
     if [ -f $preCommitFile ] && [ -z $GITHUB_ACTIONS ]
     then
-        echo '>>>>>>> Removing existing pre-commit hook...'
         rm $preCommitFile
-        echo '>>>>>>> Existing pre-commit hook removed.'
     fi
 
     if [ -f $prePushFile ] && [ -z $GITHUB_ACTIONS ]
     then
-        echo '>>>>>>> Removing existing pre-push hook...'
         rm $prePushFile
-        echo '>>>>>>> Existing pre-push hook removed.'
     fi
 
     if [ ! -f $preCommitFile ] && [ -z $GITHUB_ACTIONS ]
     then
-        echo '>>>>>>> No Talisman pre-commit hook available. Setting up the hook now..'
-        echo '>>>>>>> Copying Talisman pre-commit hook to your git hooks'
         curl https://thoughtworks.github.io/talisman/install.sh > ~/install-talisman.sh
         chmod +x ~/install-talisman.sh
         ~/install-talisman.sh pre-commit
+        echo 'pre-commit talisman hook installed'
     fi
 
     if [ ! -f $prePushFile ] && [ -z $GITHUB_ACTIONS ]
     then
-        echo '>>>>>>> No Talisman pre-push hook available. Setting up the hook now..'
-        echo '>>>>>>> Copying Talisman pre-push hook to your git hooks'
         curl https://thoughtworks.github.io/talisman/install.sh > ~/install-talisman.sh
         chmod +x ~/install-talisman.sh
         ~/install-talisman.sh pre-push
+        echo 'pre-push talisman hook installed'
     fi
 fi
