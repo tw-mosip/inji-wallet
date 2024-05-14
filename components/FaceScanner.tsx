da00f6a46cedc2635e6e24790551fbb326cc515e
adityankannan-tw <adityan410pm@gmail.com>
2024-05-14 20:52:37 +0530

    [INJIMOB-528] add logic for color and both eye crop
    Signed-off-by: adityankannan-tw <adityan410pm@gmail.com>

--- components/FaceScanner.tsx
+++ components/FaceScanner.tsx
@@ -1,12 +1,19 @@
-import React, {useCallback, useContext, useEffect, useRef, useState} from 'react';
-import {Camera, CameraType} from 'expo-camera';
+import React, {
+  useCallback,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
+import {Camera, CameraCapturedPicture, CameraType} from 'expo-camera';
 import * as FaceDetector from 'expo-face-detector';
+import * as FileSystem from 'expo-file-system';
 import {TouchableOpacity, View} from 'react-native';
 import {Button, Centered, Column, Row, Text} from './ui';
 import {useInterpret, useSelector} from '@xstate/react';
 import {useTranslation} from 'react-i18next';
 import ImageEditor from '@react-native-community/image-editor';
-import { getColors } from 'react-native-image-colors';
+import {getColors} from 'react-native-image-colors';
 import hexRgb from 'hex-rgb';
 import {closest} from 'color-diff';
 import {
@@ -26,9 +33,10 @@ import {selectIsActive} from '../machines/app';
 import {RotatingIcon} from './RotatingIcon';
 import {Theme} from './ui/styleUtils';
 import {SvgImage} from './ui/svg';
-import { Platform } from 'react-native';
-import { log } from 'xstate/lib/actions';
-import { sleep } from '../shared/commonUtil';
+import {Platform} from 'react-native';
+import {log} from 'xstate/lib/actions';
+import {sleep} from '../shared/commonUtil';
+import {color} from 'react-native-elements/dist/helpers';
 
 export const FaceScanner: React.FC<FaceScannerProps> = props => {
   const {t} = useTranslation('FaceScanner');
@@ -48,137 +56,201 @@ export const FaceScanner: React.FC<FaceScannerProps> = props => {
   const isCapturing = useSelector(service, selectIsCapturing);
   const isVerifying = useSelector(service, selectIsVerifying);
 
-  // let camera: Camera;
-  // const cameraDevice = useCameraDevice('front');
-
   const cameraRef = useRef<Camera>(Camera.Constants.Type.front);
 
-  const redColor = { red: 0, green: 0, blue: 255, alpha: 1 };
+  const [screenColor, setScreenColor] = useState('#000000');
 
-  const [screenColor, setScreenColor] = useState('#FF0000');
+  // colors extract
+  // colors match
+  // result
 
-  async function captureImage(face1) {
-    lock = true;
-    try {
-      let camoptions = {
-        mode: FaceDetector.FaceDetectorMode.accurate,
-        detectLandmarks: FaceDetector.FaceDetectorLandmarks.all,
-        runClassifications: FaceDetector.FaceDetectorClassifications.all,
-        contourMode: FaceDetector.FaceDetectorClassifications.all,
-        minDetectionInterval: 500,
-        tracking: true,
-      };
-      let faceBounds;
-      let face;
-      let faceImage;
+  let counter = 0;
+  let picArray = [{}];
+  let FaceCropPicArray = [{}];
+  let EyeCropPicArray = [{}];
+  let face;
+  let faceImage;
+  let camoptions = {
+    mode: FaceDetector.FaceDetectorMode.accurate,
+    detectLandmarks: FaceDetector.FaceDetectorLandmarks.all,
+    runClassifications: FaceDetector.FaceDetectorClassifications.all,
+    contourMode: FaceDetector.FaceDetectorClassifications.all,
+    minDetectionInterval: 500,
+    tracking: true,
+  };
 
-      if (cameraRef.current) {
-        console.log('Time before capture-->', Date.now());
-        const pic = await cameraRef.current.takePictureAsync({
-          base64: true,
-          quality: 1,
-          exif: false,
+  const MAX_COUNTER = 10; // Total number of times handleCapture will be called
+  let colorCounts = {'#0000FF': 0, '#FF0000': 0, '#00FF00': 0};
+
+  async function CropEyes() {
+    await Promise.all(
+      picArray.map(async pic => {
+        const image = pic.image;
+        face = (await FaceDetector.detectFacesAsync(image.uri, camoptions))
+          .faces[0];
+
+        let leftEyeOpenProb = face.leftEyeOpenProbability;
+        let rightEyeOpenProb = face.rightEyeOpenProbability;
+
+        if (leftEyeOpenProb > 0.85 && rightEyeOpenProb > 0.85) {
+          // face crop
+          faceImage = await ImageEditor.cropImage(image.uri, {
+            offset: {x: face.bounds.origin.x, y: face.bounds.origin.y},
+            size: {
+              width: face.bounds.size.width,
+              height: face.bounds.size.height,
+            },
+          });
+          FaceCropPicArray.push({color: pic.colour, image: faceImage});
+        } else {
+          const cachedImagePath = image.uri;
+          try {
+            await FileSystem.deleteAsync(cachedImagePath);
+            console.log('Cached image deleted successfully.');
+          } catch (error) {
+            console.error('Error deleting cached image:', error);
+          }
+        }
+      }),
+    );
+    picArray = [{}];
+
+    await Promise.all(
+      FaceCropPicArray.map(async pics => {
+        //let faceWidth = face.bounds.size.width;
+        const image = pics.image;
+        let lefteyex = face.leftEyePosition.x;
+        let righteyex = face.rightEyePosition.x;
+        let lefteyey = face.leftEyePosition.y;
+        let righteyey = face.rightEyePosition.y;
+
+        let offsetX = 200;
+        let offsetY = 350;
+        let leftcroppedImage;
+        let rightcroppedImage;
+        // left eye crop
+        leftcroppedImage = await ImageEditor.cropImage(image.uri, {
+          offset: {
+            x: lefteyex - offsetX,
+            y: lefteyey - offsetY,
+          },
+          size: {width: offsetX * 2, height: offsetY / 2 - 50},
         });
 
-        if (pic !== null && pic !== undefined) {
-          face = (await FaceDetector.detectFacesAsync(pic.uri, camoptions))
-            .faces[0];
-          console.log('Captured face points', face);
-        }
+        rightcroppedImage = await ImageEditor.cropImage(image.uri, {
+          offset: {
+            x: righteyex - offsetX,
+            y: righteyey - offsetY,
+          },
+          size: {width: offsetX * 2, height: offsetY / 2 - 50},
+        });
+        //console.log('Cropped image uri-->', croppedImage.uri);
+        EyeCropPicArray.push({
+          color: pics.color,
+          leftEye: leftcroppedImage,
+          rightEye: rightcroppedImage,
+        });
+      }),
+    );
+    FaceCropPicArray = [{}];
 
-        // if(faceBounds !== null && faceBounds !== undefined) {
-        //   console.log('Inside faceBounds');
-        //   faceImage = await ImageEditor.cropImage(pic.uri, {
-        //     offset: { x: faceBounds.bounds.origin.x, y: faceBounds.bounds.origin.y },
-        //     size: { width: faceBounds.bounds.size.width, height: faceBounds.bounds.size.height },
-        //   });
+    if (Platform.OS == 'ios') {
+      await Promise.all(
+        EyeCropPicArray.map(async pics => {
+          const color = pics.color;
+          const leftEye = pics.leftEye;
+          const rightEye = pics.rightEye;
 
-        //   console.log('Face Image', faceImage.uri);
+          const leftEyeColors = await getColors(leftEye.uri);
+          const rightEyeColors = await getColors(rightEye.uri);
 
-        // if (faceImage !== null && faceImage !== undefined) {
-        //   face = (await FaceDetector.detectFacesAsync(faceImage.uri, camoptions)).faces[0];
-        //   console.log('Cropped face points', face);
+          console.log(
+            'Left eye colors --->',
+            leftEyeColors,
+            'and color is-->',
+            color,
+          );
+          console.log(
+            'Right eye colors --->',
+            rightEyeColors,
+            'and color is-->',
+            color,
+          );
 
-        // if (face !== null && face !== undefined) {
+          let LeftrgbaColors = Object.values(leftEyeColors)
+            .filter(color => typeof color === 'string' && color.startsWith('#'))
+            .map(color => hexRgb(color));
+          console.log('RGB left are ->', LeftrgbaColors);
 
-        let colors;
-        let rgbaColors;
-        let redsum = 0;
-        let greensum = 0;
-        let bluesum = 0;
+          let RightrgbaColors = Object.values(rightEyeColors)
+            .filter(color => typeof color === 'string' && color.startsWith('#'))
+            .map(color => hexRgb(color));
+          console.log('RGB left are ->', RightrgbaColors);
+        }),
+      );
+    }
+    // let rgbaColors;
+    // let redsum = 0;
+    // let greensum = 0;
+    // let bluesum = 0;
+  }
 
-        let faceWidth = face.bounds.size.width;
-        let scaleFactor = pic.width / faceWidth;
-
-        let lefteyex = face.leftEyePosition.x;
-        let lefteyey = face.leftEyePosition.y;
-        let offsetX = 200;
-
-        if (face !== null && face !== undefined) {
-          let croppedImage = await ImageEditor.cropImage(pic.uri, {
-            offset: {
-              x: lefteyex * scaleFactor - offsetX,
-              y: lefteyey * scaleFactor - offsetX,
-            },
-            size: {width: offsetX * 2, height: offsetX - 50},
+  async function captureImage(screenColor) {
+    try {
+      if (cameraRef.current) {
+        await cameraRef.current
+          .takePictureAsync({
+            base64: true,
+            quality: 1,
+            exif: false,
+          })
+          .then(resp => {
+            console.log('Captured Image-->', resp.uri);
+            picArray.push({color: screenColor, image: resp});
           });
-          console.log('Cropped image uri-->', croppedImage.uri);
 
-          if (Platform.OS == 'ios') {
-            colors = await getColors(croppedImage.uri);
-            console.log('Colors are ->', colors);
+        //   if (Platform.OS == 'ios') {
+        //     colors = await getColors(croppedImage.uri);
+        //     console.log('Colors are ->', colors);
 
-            rgbaColors = Object.values(colors)
-              .filter(
-                color => typeof color === 'string' && color.startsWith('#'),
-              )
-              .map(color => hexRgb(color));
+        //     rgbaColors = Object.values(colors)
+        //       .filter(
+        //         color => typeof color === 'string' && color.startsWith('#'),
+        //       )
+        //       .map(color => hexRgb(color));
 
-            console.log('RGB colors are ->', rgbaColors);
+        //     console.log('RGB colors are ->', rgbaColors);
 
-            rgbaColors.forEach(color => {
-              redsum += color.red;
-              greensum += color.green;
-              bluesum += color.blue;
-            });
+        //     rgbaColors.forEach(color => {
+        //       redsum += color.red;
+        //       greensum += color.green;
+        //       bluesum += color.blue;
+        //     });
 
-            console.log('red-->', redsum);
-            console.log('blue-->', bluesum);
-            console.log('green-->', greensum);
-          }
-        } else {
-          console.log('No face detected in the cropped image.');
-        }
-      } else {
-        console.log('Face image could not be generated.');
+        //     console.log('red-->', redsum);
+        //     console.log('blue-->', bluesum);
+        //     console.log('green-->', greensum);
+        //   }
+        //   else {
+        //     console.log('No face detected in the cropped image.');
+        //   }
       }
-      setScreenColor('#0000FF');
     } catch (error) {
       console.error('Error capturing image:', error);
     }
-    lock = false;
   }
-  
-let captureCount = 0;
-let lock = false;
-
-async function lockFlow(face){
-  if(lock){
-    setTimeout(async ()=> {
-      await lockFlow(face);
-    },200);
-    return;
-  }
-  await captureImage(face);
-}
 
   const handleFacesDetected = async ({faces}) => {
-    if (faces.length > 0) {
-      console.log("Time when the face is detected-->", Date.now());
-      //DeviceBrightness.setBrightnessLevel(1);
+    if (counter > MAX_COUNTER) {
+      counter -= 10;
+      cameraRef.current.pausePreview();
+      setScreenColor('#000000');
+      await CropEyes();
+      return;
+    } else if (faces.length > 0) {
       const {bounds, yawAngle, rollAngle} = faces[0];
 
-      console.log('facessss-->', faces[0]);
+      // console.log('facessss-->', faces[0]);
 
       const withinXBounds =
         bounds.origin.x + bounds.size.width >= 310 && bounds.origin.x <= 320;
@@ -186,102 +258,113 @@ async function lockFlow(face){
         bounds.origin.y + bounds.size.height >= 310 && bounds.origin.y <= 320;
       const withinYawAngle = yawAngle > -10 && yawAngle < 10;
       const withinRollAngle = yawAngle > -3 && rollAngle < 3;
-      
-      if (withinXBounds && withinYBounds && withinRollAngle && withinYawAngle && captureCount < 2 ) {
-        captureCount += 1;
-        lockFlow(faces[0]);
-        // await captureImage(faces[0]);
-        // red, blue, yellow, 
+
+      if (
+        withinXBounds &&
+        withinYBounds &&
+        withinRollAngle &&
+        withinYawAngle &&
+        counter < MAX_COUNTER
+      ) {
+        const colors = Object.keys(colorCounts);
+        const randomColor = colors[Math.floor(Math.random() * colors.length)];
+
+        if (colorCounts[randomColor] < 5) {
+          setScreenColor(randomColor); // Set the screen color
+          counter += 1; // Increment the counter
+          colorCounts[randomColor]++;
+          await captureImage(screenColor);
+        }
       }
     }
-  };
 
-  const setCameraRef = useCallback(
-    (node: Camera) => {
-      if (node != null && !isScanning) {
-        service.send(FaceScannerEvents.READY(node));
+    const setCameraRef = useCallback(
+      (node: Camera) => {
+        if (node != null && !isScanning) {
+          service.send(FaceScannerEvents.READY(node));
+        }
+      },
+      [isScanning],
+    );
+
+    useEffect(() => {
+      if (isValid) {
+        props.onValid();
+      } else if (isInvalid) {
+        props.onInvalid();
       }
-    },
-    [isScanning],
-  );
+    }, [isValid, isInvalid]);
 
-  useEffect(() => {
-    if (isValid) {
-      props.onValid();
-    } else if (isInvalid) {
-      props.onInvalid();
+    useEffect(() => {
+      if (isActive) {
+        service.send(FaceScannerEvents.APP_FOCUSED());
+      }
+    }, [isActive]);
+
+    if (isCheckingPermission) {
+      return <Column></Column>;
+    } else if (isPermissionDenied) {
+      return (
+        <Column padding="24" fill align="space-between">
+          <Text align="center" color={Theme.Colors.errorMessage}>
+            {t('missingPermissionText')}
+          </Text>
+          <Button
+            title={t('allowCameraButton')}
+            onPress={() => service.send(FaceScannerEvents.OPEN_SETTINGS())}
+          />
+        </Column>
+      );
     }
-  }, [isValid, isInvalid]);
 
-  useEffect(() => {
-    if (isActive) {
-      service.send(FaceScannerEvents.APP_FOCUSED());
-    }
-  }, [isActive]);
-
-  if (isCheckingPermission) {
-    return <Column></Column>;
-  } else if (isPermissionDenied) {
     return (
-      <Column padding="24" fill align="space-between">
-        <Text align="center" color={Theme.Colors.errorMessage}>
-          {t('missingPermissionText')}
-        </Text>
-        <Button
-          title={t('allowCameraButton')}
-          onPress={() => service.send(FaceScannerEvents.OPEN_SETTINGS())}
-        />
+      <Column fill align="space-between" style={{backgroundColor: screenColor}}>
+        <View style={Theme.Styles.scannerContainer}>
+          <Camera
+            style={Theme.Styles.scanner}
+            type={whichCamera}
+            ref={cameraRef}
+            onFacesDetected={handleFacesDetected}
+            faceDetectorSettings={{
+              mode: FaceDetector.FaceDetectorMode.accurate,
+              detectLandmarks: FaceDetector.FaceDetectorLandmarks.all,
+              runClassifications: FaceDetector.FaceDetectorClassifications.all,
+              contourMode: FaceDetector.FaceDetectorClassifications.all,
+              minDetectionInterval: 1000,
+              tracking: true,
+            }}
+          />
+        </View>
+        <Centered>
+          {isCapturing || isVerifying ? (
+            <RotatingIcon name="sync" size={64} />
+          ) : (
+            <Row align="center">
+              <Centered style={Theme.Styles.imageCaptureButton}>
+                <TouchableOpacity
+                  onPress={() => service.send(FaceScannerEvents.CAPTURE())}>
+                  {SvgImage.CameraCaptureIcon()}
+                </TouchableOpacity>
+                <Text size="small" weight="semibold" margin="8">
+                  {t('capture')}
+                </Text>
+              </Centered>
+
+              <Centered>
+                <TouchableOpacity
+                  onPress={() => service.send(FaceScannerEvents.FLIP_CAMERA())}>
+                  {SvgImage.FlipCameraIcon()}
+                </TouchableOpacity>
+                <Text size="smaller" weight="semibold" margin="8">
+                  {t('flipCamera')}
+                </Text>
+              </Centered>
+            </Row>
+          )}
+        </Centered>
       </Column>
     );
-  }
-
-  return (
-    <Column fill align="space-between" style={{backgroundColor: screenColor}}>
-      <View style={Theme.Styles.scannerContainer}>
-        <Camera
-          style={Theme.Styles.scanner}
-          type={whichCamera}
-          ref={cameraRef}
-          onFacesDetected={handleFacesDetected}
-          faceDetectorSettings={{
-            mode: FaceDetector.FaceDetectorMode.accurate,
-            detectLandmarks: FaceDetector.FaceDetectorLandmarks.all,
-            runClassifications: FaceDetector.FaceDetectorClassifications.all,
-            contourMode: FaceDetector.FaceDetectorClassifications.all,
-            minDetectionInterval: 1000,
-            tracking: true
-          }}
-        />
-      </View>
-      <Centered>
-        {isCapturing || isVerifying ? (
-          <RotatingIcon name="sync" size={64} />
-        ) : (
-          <Row align="center">
-            <Centered style={Theme.Styles.imageCaptureButton}>
-              <TouchableOpacity
-                onPress={() => service.send(FaceScannerEvents.CAPTURE())}>
-                {SvgImage.CameraCaptureIcon()}
-              </TouchableOpacity>
-              <Text size="small" weight="semibold" margin="8">
-                {t('capture')}
-              </Text>
-            </Centered>
-
-            <Centered>
-              <TouchableOpacity
-                onPress={() => service.send(FaceScannerEvents.FLIP_CAMERA())}>
-                {SvgImage.FlipCameraIcon()}
-              </TouchableOpacity>
-              <Text size="smaller" weight="semibold" margin="8">
-                {t('flipCamera')}
-              </Text>
-            </Centered>
-          </Row>
-        )}
-      </Centered>
-    </Column>
-  );
+  };
 };
 
 interface FaceScannerProps {
